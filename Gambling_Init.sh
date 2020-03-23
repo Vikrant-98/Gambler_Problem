b3251861b9704d20e4b98825c868cbeef041a3f8
Vikrant-98 <vikrantchitte5398@gmail.com>
2020-03-23 19:29:14 +0530

    Gambler Wins or Looses

--- Gambling_Init.sh
+++ Gambling_Init.sh
@@ -2,3 +2,12 @@
 
 gambler=100
 gambler_bet=1
+val=$((RANDOM%2))
+
+        if [ $val -eq 1 ]
+        then
+                gambler=$(($gambler+$gambler_bet))
+        else
+                gambler=$(($gambler-$gambler_bet))
+        fi
+
