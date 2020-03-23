8555c5930ee9388f42a88053d3a09bcf8f7c13bd
Vikrant-98 <vikrantchitte5398@gmail.com>
2020-03-23 19:37:46 +0530

    Gambler Quit when Wins or Looses 50%

--- Gambling_Init.sh
+++ Gambling_Init.sh
@@ -3,11 +3,17 @@
 gambler=100
 gambler_bet=1
 val=$((RANDOM%2))
+Gambler_Stake_Win=$(($gambler+$gambler/2))
+Gambler_Stake_Loose=$(($gambler-$gambler/2))
+while [[ $gambler != $Gambler_Stake_Win && $gambler != $Gambler_Stake_Loose  ]]
+do
+val=$((RANDOM%2))
 
-        if [ $val -eq 1 ]
+        if [[ $val == 1 ]]
         then
                 gambler=$(($gambler+$gambler_bet))
-        else
+        elif [[ $val == 0 ]]
+	then
                 gambler=$(($gambler-$gambler_bet))
         fi
-
+done
