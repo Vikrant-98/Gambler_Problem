7078191e53ad81a4a0ac3191e0e64a2016fc1954
Vikrant-98 <vikrantchitte5398@gmail.com>
2020-03-23 19:49:08 +0530

    Total amount count for 20 days

--- Gambling_Init.sh
+++ Gambling_Init.sh
@@ -1,19 +1,34 @@
 #!/bin/bash -x
 
 gambler=100
-gambler_bet=1
-val=$((RANDOM%2))
+Gambler_bet=1
 Gambler_Stake_Win=$(($gambler+$gambler/2))
 Gambler_Stake_Loose=$(($gambler-$gambler/2))
-while [[ $gambler != $Gambler_Stake_Win && $gambler != $Gambler_Stake_Loose  ]]
+Gambler_Fix=100
+gambler_Total_Money=0
+day_Count=0
+count1=0
+count2=0
+while [ $day_Count != 20 ]
+do
+((day_Count++))
+gambler=$Gambler_Fix
+while [[ $gambler != $Gambler_Stake_Win && $gambler != $Gambler_Stake_Loose ]]
 do
 val=$((RANDOM%2))
-
         if [[ $val == 1 ]]
         then
-                gambler=$(($gambler+$gambler_bet))
-        elif [[ $val == 0 ]]
+                gambler=$(($gambler+$Gambler_bet))
+        	((count1++))
+	elif [[ $val == 0 ]]
 	then
-                gambler=$(($gambler-$gambler_bet))
-        fi
+                gambler=$(($gambler-$Gambler_bet))
+        	((count2++))
+	fi
+gambler_Total_Money=$(($gambler_Total_Money+$gambler-$Gambler_Fix))
 done
+done
+
+echo "$gambler_Total_Money"
+echo "$count1"
+echo "$count2"
