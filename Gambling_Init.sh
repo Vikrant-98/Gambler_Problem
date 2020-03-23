e8c4b4dac45a9ef0662af6c1c6310e19de240cd2
Vikrant-98 <vikrantchitte5398@gmail.com>
2020-03-23 20:30:20 +0530

    Refactor : Change total winning amount

--- Gambling_Init.sh
+++ Gambling_Init.sh
@@ -7,25 +7,30 @@ Gambler_Stake_Loose=$(($gambler-$gambler/2))
 Gambler_Fix=100
 gambler_Total_Money=0
 day_Count=0
-count1=0
-count2=0
 while [ $day_Count != 20 ]
 do
 ((day_Count++))
 gambler=$Gambler_Fix
-while [[ $gambler != $Gambler_Stake_Win && $gambler != $Gambler_Stake_Loose ]]
-do
-val=$((RANDOM%2))
-        if [[ $val == 1 ]]
-        then
-                gambler=$(($gambler+$Gambler_bet))
-        	((count1++))
-	elif [[ $val == 0 ]]
+	while [[ $gambler != $Gambler_Stake_Win && $gambler != $Gambler_Stake_Loose ]]
+	do
+		val=$((RANDOM%2))
+        	if [[ $val == 1 ]]
+        	then
+                	gambler=$(($gambler+$Gambler_bet))
+		elif [[ $val == 0 ]]
+		then
+                	gambler=$(($gambler-$Gambler_bet))
+		fi
+	done
+gambler_Total_Money=$(( $gambler_Total_Money + ($gambler-$Gambler_Fix) ))
+
+	if [ $gambler_Total_Money > 0 ]
 	then
-                gambler=$(($gambler-$Gambler_bet))
-        	((count2++))
+		echo "Gambler wins $gambler_Total_Money!!!!!!!!!!!!!!!!!!!!!!!"
+	elif [ $gambler_Total_Money  <= 0 ]
+	then
+		temp=$(( 2*$gambler_Total_Money - $gambler_Total_Money ))
+		echo "Gambler looses $temp!!!!!!!!!!!!!!!!!!!!!!"
 	fi
-gambler_Total_Money=$(($gambler_Total_Money+$gambler-$Gambler_Fix))
-done
 done
 
